f7bbc3e491607c6278bb6c05ec90f8f25ed50275
thiagoa1 <thiago.monkey@gmail.com>
2019-10-07 18:57:23 -0300

    Início de spawn de inimigos

--- game.lua
+++ game.lua
@@ -1,11 +1,12 @@
-local composer = require( "composer" )
-
 system.activate( "multitouch" )
+local physics = require( "physics" )
+physics.start()
+physics.setGravity( 0, 0 )
 
+local composer = require("composer")
 local scene = composer.newScene()
 
-local charOptions =
-{
+local charOptions = {
     width = 71,
     height = 67,
     numFrames = 4,
@@ -13,9 +14,148 @@ local charOptions =
     sheetContentHeight = 134
 }
 
+local char8Options = {
+    width = 71,
+    height = 67,
+    numFrames = 8,
+    sheetContentWidth = 284,
+    sheetContentHeight = 134
+}
+
 local char_idle = graphics.newImageSheet( "player/idle/idle.png", charOptions )
 
 local char_jump = graphics.newImageSheet( "player/jump/jump.png", charOptions )
+
+-- ============================== Spawn de inimigos
+local spawnTimer
+local spawnedObjects = {}
+-- Seed the random number generator
+math.randomseed( os.time() )
+
+local blueEnemy_run = graphics.newImageSheet( "player/run/run_blue.png", char8Options )
+local greenEnemy_run = graphics.newImageSheet( "player/run/run_green.png", char8Options )
+
+local enemy_sequence = {
+    -- consecutive frames sequence
+    {
+        name = "run",
+        sheet = char_idle,
+        start = 1,
+        count = 8,
+        time = 500,
+        loopCount = 0,
+        loopDirection = "forward"
+    }
+}
+
+local spawnParams = {
+    xMin = 20,
+    xMax = 300,
+    yMin = 20,
+    yMax = 460,
+    spawnTime = 500,
+    spawnOnTimer = 12,
+    spawnInitial = 4
+}
+
+--local bounds = {
+--    xMin = 0,
+--    xMax = display.contentWidth,
+--    yMin = 0,
+--    yMax = display.contentHeight,
+--}
+
+local function onLocalPreCollision( self, event )
+   -- if ( event.phase == "began" ) then
+        display.remove( self )
+        print( self.name .. ": precollision with " .. event.other.name )
+
+ 
+--    elseif ( event.phase == "ended" ) then
+--        print( self.name .. ": collision ended with " .. event.other.name )
+    --end
+end
+
+local enemyCount = 0
+
+-- Spawn an enemy
+local function spawnBlueEnemy()
+
+    -- Create an item
+    local blueEnemy = display.newSprite( blueEnemy_run, enemy_sequence )
+
+--    print("bounds: ", bounds.xMin, bounds.xMax, bounds.yMin, bounds.yMax)
+
+    -- Position item randomly within set bounds
+    --blueEnemy.x = math.random( bounds.xMin, bounds.xMax )
+    --blueEnemy.y = math.random( bounds.yMin, bounds.yMax )
+    blueEnemy.x = display.screenOriginX
+    blueEnemy.y = display.contentCenterY + 80
+    blueEnemy.xScale = 4
+    blueEnemy.yScale = 4
+    blueEnemy.name = "blueEnemy" .. enemyCount
+    --idleChar.x = display.contentCenterX
+    --idleChar.y = display.contentCenterY + 80
+    blueEnemy:play()
+
+    physics.addBody( blueEnemy, { density=3.0, friction=0.5, bounce=0.3 } )
+    blueEnemy.preCollision  = onLocalPreCollision
+    blueEnemy:addEventListener( "preCollision" )
+
+    transition.to(blueEnemy, {x=display.contentCenterX, y=display.contentCenterY + 80, time=1000})
+    
+    -- Add item to "spawnedObjects" table for tracking purposes
+    spawnedObjects[enemyCount] = blueEnemy
+    enemyCount = enemyCount + 1
+end
+
+local function spawnController( action, params )
+ 
+    -- Cancel timer on "start" or "stop", if it exists
+    if ( spawnTimer and ( action == "start" or action == "stop" ) ) then
+        timer.cancel( spawnTimer )
+    end
+ 
+    -- Start spawning
+    if ( action == "start" ) then
+ 
+        -- Gather/set spawning bounds
+        local spawnBounds = {}
+        spawnBounds.xMin = params.xMin or 0
+        spawnBounds.xMax = params.xMax or display.actualContentWidth
+        spawnBounds.yMin = params.yMin or 0
+        spawnBounds.yMax = params.yMax or display.actualContentHeight
+ 
+        -- Gather/set other spawning params
+        local spawnTime = params.spawnTime or 1000
+        local spawnOnTimer = params.spawnOnTimer or 50
+        local spawnInitial = params.spawnInitial or 0
+ 
+        -- If "spawnInitial" is greater than 0, spawn that many item(s) instantly
+        if ( spawnInitial > 0 ) then
+            for n = 1,spawnInitial do
+                spawnBlueEnemy( )
+            end
+        end
+ 
+        -- Start repeating timer to spawn items
+        if ( spawnOnTimer > 0 ) then
+            spawnTimer = timer.performWithDelay( spawnTime,
+                function() spawnBlueEnemy(); end,
+            spawnOnTimer )
+        end
+ 
+    -- Pause spawning
+    elseif ( action == "pause" ) then
+        timer.pause( spawnTimer )
+ 
+    -- Resume spawning
+    elseif ( action == "resume" ) then
+        timer.resume( spawnTimer )
+    end
+end
+
+-- ============================== Spawn de inimigos
 
 local char_sequence = {
     -- consecutive frames sequence
@@ -64,7 +204,7 @@ function scene:create( event )
     local sceneGroup = self.view
     -- Code here runs when the scene is first created but has not yet appeared on screen
 
-    local background = display.newImageRect(sceneGroup, "img/background.png", 1280, 800 )
+    local background = display.newImageRect(sceneGroup, "img/background.png", display.actualContentWidth, display.actualContentHeight )
 
     background.x = display.contentCenterX
     background.y = display.contentCenterY
@@ -73,8 +213,13 @@ function scene:create( event )
     idleChar.yScale = 4
     idleChar.x = display.contentCenterX
     idleChar.y = display.contentCenterY + 80
+    idleChar.name = "char"
     idleChar:play()
 
+    physics.addBody( idleChar, { density=3.0, friction=100, bounce=0 } )
+
+    -- spawnBlueEnemy()
+    spawnController( "start", spawnParams )
 end
 
 local function rightTapListener( event )
--- menu.lua
+++ menu.lua
@@ -1,11 +1,12 @@
 local composer = require( "composer" )
+local fx = require( "com.ponywolf.ponyfx" )
 
 local scene = composer.newScene()
 
 local function gotoGame()
     composer.gotoScene( "game" )
 end
- 
+
 local function gotoHighScores()
     composer.gotoScene( "highscores" )
 end
@@ -14,7 +15,11 @@ function scene:create( event )
     local sceneGroup = self.view
     -- Code here runs when the scene is first created but has not yet appeared on screen
 
-    local background = display.newImageRect(sceneGroup, "img/background.png", 1280, 800 )
+    bgMusic = audio.loadStream( "sfx/menu.wav" )
+    -- bgMusic = audio.loadSound( "sfx/menu.wav" )
+
+
+    local background = display.newImageRect(sceneGroup, "img/background.png", display.actualContentWidth, display.actualContentHeight )
 --    background.fill.effect = "filter.blur"
     --background.fill.effect = "filter.blur"
     background.fill.effect = "filter.desaturate"
@@ -28,15 +33,57 @@ function scene:create( event )
     title.x = display.contentCenterX
     title.y = 200
 
-    local playButton = display.newText( sceneGroup, "Jogar", display.contentCenterX, display.contentCenterY + 50, native.systemFont, 44 )
+    local playButton = display.newText( sceneGroup, "Jogar", display.contentCenterX, display.contentCenterY + 80, native.systemFont, 44 )
     playButton:setFillColor( 1, 0.53, 0.96 )
     playButton:addEventListener( "tap", gotoGame )
  
-    local rankingButton = display.newText( sceneGroup, "Ranking", display.contentCenterX, display.contentCenterY + 130, native.systemFont, 44 )
+    local rankingButton = display.newText( sceneGroup, "Ranking", display.contentCenterX, display.contentCenterY + 160, native.systemFont, 44 )
     rankingButton:setFillColor( 1, 0.53, 0.96 )
     --rankingButton:addEventListener( "tap", gotoHighScores )
 end
 
+
+local function enterFrame( event )
+
+	local elapsed = event.time
+
+end
+
+-- This function is called when scene comes fully on screen
+function scene:show( event )
+	local phase = event.phase
+	if ( phase == "will" ) then
+		fx.fadeIn()
+		-- add enterFrame listener
+		--Runtime:addEventListener( "enterFrame", enterFrame )
+	elseif ( phase == "did" ) then
+		timer.performWithDelay( 10, function()
+			audio.play( bgMusic, { loops = -1, channel = 1 } )
+			audio.fade({ channel = 1, time = 333, volume = 1.0 } )
+		end)
+	end
+end
+
+-- This function is called when scene goes fully off screen
+function scene:hide( event )
+	local phase = event.phase
+	if ( phase == "will" ) then
+		--start:removeEventListener( "tap" )
+		audio.fadeOut( { channel = 1, time = 1500 } )
+	elseif ( phase == "did" ) then
+		Runtime:removeEventListener( "enterFrame", enterFrame )
+	end
+end
+
+function scene:destroy( event )
+	audio.stop()  -- Stop all audio
+	audio.dispose( bgMusic )  -- Release music handle
+--s	Runtime:removeEventListener("key", key)
+end             
+
 scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
 
 return scene
